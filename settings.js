00e28dbc266427e5dfbc81a1d2069c43d1a30af9
raaazy <147167055+raaazy@users.noreply.github.com>
2026-03-18 13:14:34 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~WiS8aa", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "254743443368", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
